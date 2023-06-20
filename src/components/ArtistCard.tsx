e0b45eec696ea2ec03451617193194315867ee67
Ko-Eunseo <koyk0408@naver.com>
2023-06-20 17:23:48 +0900

    design: 아티스트 카드 컴포넌트

--- src/components/ArtistCard.tsx
+++ src/components/ArtistCard.tsx
@@ -1,21 +1,27 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { ArtistData } from "./ArtistWrap";
+import { ArtistData } from "./ArtistList";
+import { AiOutlineHeart } from "react-icons/ai";
 
 export default function ArtistCard({ artist }: { artist: ArtistData }) {
   return (
     <Link
       href={`/artist/${artist._id}`}
-      className="group rounded-xl flex flex-col items-center overflow-hidden bg-[#f2f2f2] p-2 hover:scale-105 duration-300 ease-in-out"
+      className="group rounded-xl flex flex-col items-center overflow-hidden bg-white p-2 hover:scale-105 duration-300 ease-in-out"
     >
-      <Image
-        className="max-h-[300px]"
-        src={artist.profile}
-        alt={artist.korName}
-        width={300}
-        height={500}
-      />
+      <div
+        className="relative w-300 h-300 bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${artist.profile})`,
+          width: "250px",
+          height: "250px",
+        }}
+      >
+        <div className="absolute top-0 right-0 p-2">
+          <AiOutlineHeart className="text-2xl hover:text-red-400 duration-300 ease-in-out" />
+        </div>
+      </div>
       <p className="text-lg font-bold text-center text-black mt-2 group-hover:tracking-widest group-hover:text-red-400 duration-300 ease-in-out">
         {artist.korName ? artist.korName : artist.enName}
       </p>
