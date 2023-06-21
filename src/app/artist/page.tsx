c636f41aa51f3b3e8835412b95d48081e564572d
Ko-Eunseo <koyk0408@naver.com>
2023-06-21 22:25:59 +0900

    feat: 무한스크롤 hook 작성, 아티스트 무한스크롤 페이지 구현

--- src/app/artist/page.tsx
+++ src/app/artist/page.tsx
@@ -1,17 +1,25 @@
 "use client";
 
 import ArtistList from "@/components/ArtistList";
+import ArtistInfiniteScroll from "@/components/ArtistInfiniteScroll";
 import SearchBox from "@/components/SearchBox";
 import Title from "@/components/common/Title";
 import React, { useState } from "react";
 
 export default function Page() {
   const [keyword, setKeyword] = useState<string | null>("");
+
   return (
     <div>
       <SearchBox setKeyword={setKeyword} />
+      {keyword && (
+        <>
+          <Title>{keyword} 검색 결과</Title>
+          <ArtistList keyword={keyword} />
+        </>
+      )}
       <Title>All Artists</Title>
-      <ArtistList keyword={keyword} />
+      <ArtistInfiniteScroll />
     </div>
   );
 }
