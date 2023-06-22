6812a41ef63c7c31cb48692020566582a1a07e9f
Mintaek_CHO <mintaek5555@gmail.com>
2023-06-22 19:29:27 +0900

    refactor: 유튜브 key 만료시 새로운 key 사용하도록 수정

--- src/service/youtube/YoutubeClient.js
+++ src/service/youtube/YoutubeClient.js
@@ -5,7 +5,6 @@ export default class YoutubeClient {
   constructor() {
     this.httpClient = axios.create({
       baseURL: "https://www.googleapis.com/youtube/v3",
-      params: { key: process.env.NEXT_PUBLIC_YOUTUBE_API_KEY },
     });
   }
 
