27592638129a0eb39f2334330ff7f6c480c895ab
Mintaek_CHO <mintaek5555@gmail.com>
2023-06-23 10:43:48 +0900

    refactor : 현재 공연중인 콘서트 조회 API 로직 수정

--- src/app/api/consert/proceeding/route.ts
+++ src/app/api/consert/proceeding/route.ts
@@ -4,28 +4,26 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   const name = request.nextUrl.searchParams.get("name");
+
   try {
-    console.log(process.env.MONGODB_URI);
     dbConnect();
     const conserts = Consert;
 
     const date = new Date();
-    const today = date.toString().split("T");
     const year = date.getFullYear().toString();
 
     const allConserts = await conserts
       .find()
       .or([{ cast: { $regex: name } }, { title: { $regex: name } }])
       .and([{ date: { $regex: year } }])
-      .or([{ date: { $regex: year + 1 } }]);
       if(allConserts.length === 0) return NextResponse.json(allConserts)
     const response = allConserts.filter(
-      (consert) => consert.date.split(" ~ ")[0] >= today
+      (consert) => new Date(consert.date.split(" ~ ")[1]) >= date
     );
-    console.log(response, "1");
+    console.log(allConserts, "1");
     return NextResponse.json(response);
   } catch (error) {
     console.error(error);
-    NextResponse.json({ message: "Internal server error" });
+    return NextResponse.json({ message: "Internal server error" });
   }
 }
