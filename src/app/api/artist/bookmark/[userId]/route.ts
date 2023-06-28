c522fe0895bf334ddd989ea26eb0565ce3e36c7f
Mintaek_CHO <mintaek5555@gmail.com>
2023-06-28 13:58:12 +0900

    feat: 홈페이지 My-Choice 북마크 API와 맵핑 후 렌더링 - 무한 스크롤 적용 여부는 논이 후 결정

--- src/app/api/artist/bookmark/[userId]/route.ts
+++ src/app/api/artist/bookmark/[userId]/route.ts
@@ -2,21 +2,21 @@ import dbConnect from "@/db/dbConnect";
 import Bookmark from "@/db/schema/bookmark";
 import { NextRequest, NextResponse } from "next/server";
 type Props = {
-    params: {
-        userId: string;
-    }
-}
+  params: {
+    userId: string;
+  };
+};
 
-export async function GET(request: NextRequest, {params: {userId}}: Props) {
-    try {
-      dbConnect();
-      const bookmark = await Bookmark.find({
-        userId: userId,
-      });
-  
-      return NextResponse.json(bookmark);
-    } catch (error) {
-      console.error(error);
-      NextResponse.json({ message: "Internal server error" });
-    }
+export async function GET(request: NextRequest, { params: { userId } }: Props) {
+  try {
+    dbConnect();
+    const bookmark = await Bookmark.find({
+      userId: userId,
+    });
+
+    return NextResponse.json(bookmark);
+  } catch (error) {
+    console.error(error);
+    NextResponse.json({ message: "Internal server error" });
   }
+}
--- src/components/organisms/MyChoice.tsx
+++ src/components/organisms/MyChoice.tsx
@@ -1,12 +1,32 @@
-'use client'
-import React from "react";
-import ArtistInfiniteScroll from "../ArtistInfiniteScroll";
+"use client";
+import { BookmarkData } from "@/types/_type";
+import { useSession } from "next-auth/react";
+import React, { useRef } from "react";
+import ArtistCard from "../molecules/ArtistCard";
+import useSWR from "swr";
+import Artist from "@/service/artist/Artist";
 
 export default function MyChoice() {
+  const target = useRef<HTMLDivElement>(null);
+  const { data: session } = useSession();
+  const artistApi = new Artist();
+
+  const { isLoading, error, data } = useSWR(
+    `/api/artist/bookmark/${session?.user.id}`,
+    () => artistApi.getUserBookmark(session?.user.id as string)
+  );
+
+  const bookmarks: BookmarkData[] = data?.data;
   return (
     <section className="my-8">
       <h2 className="text-2xl font-bold mb-4">My-Choice</h2>
-      <ArtistInfiniteScroll />
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        {bookmarks?.map((bookmark) => (
+          <li key={bookmark._id}>
+            <ArtistCard artist={bookmark.artist} />
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
