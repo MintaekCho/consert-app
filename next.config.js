435b781c28a4d6512f56245476b64a9c924263ba
Mintaek_CHO <mintaek5555@gmail.com>
2023-06-30 23:31:53 +0900

    Fix: CORS 이슈 테스트

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "/:path*",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/:path*",
+  //       destination: "/:path*",
+  //     },
+  //   ];
+  // },
   images: {
     domains: [
       "search.pstatic.net",
