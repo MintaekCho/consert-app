a81cc82488dee4c1b0080e3d2bd660be26bfb55c
Mintaek_CHO <mintaek5555@gmail.com>
2023-07-01 08:49:01 +0900

    Fix: Conser Detail 쿼리 에러 수정

--- src/app/api/consert/details/route.ts
+++ src/app/api/consert/details/route.ts
@@ -4,11 +4,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   const consertId = request.nextUrl.searchParams.get("consertId");
+  console.log(consertId)
   try {
     dbConnect();
     const conserts = Consert;
 
-    const resConsert = await conserts.findOne({consertId: consertId})
+    const resConsert = await conserts.findOne({ _id: consertId });
     console.log(resConsert);
     return NextResponse.json(resConsert);
   } catch (error) {
