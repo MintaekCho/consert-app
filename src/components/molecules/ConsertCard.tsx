a492674fc5f3c6afd741c23d5a4add274e009d96
Mintaek_CHO <mintaek5555@gmail.com>
2023-07-02 12:16:39 +0900

    Chore: 타입 오타 수정

---
+++ src/components/molecules/ConsertCard.tsx
@@ -0,0 +1,23 @@
+import { ConcertData, Rank, RecentConsertData } from "@/types/_type";
+import Image from "next/image";
+import React from "react";
+
+export default function ConsertCard({
+  consert,
+}: {
+  consert: Rank | ConcertData | RecentConsertData;
+}) {
+  return (
+    <article className="w-full max-w-[250px] flex flex-col gap-2 items-center justify-center hover:scale-105 duration-300 ease-in-out">
+        <Image
+        // className="min-w-[230px] max-h-[280px]"
+          src={consert.image}
+          alt={consert.title}
+          width={250}
+          height={450}
+        />
+      <p className="w-full font text-md md:text-lg lg:text-xl xl:text-2xl font-bold text-center truncate">{consert.title}</p>
+      <p className="w-full truncate text-xs md:text-sm xl:text-md font-bold opacity-70 text-center">{consert.place}</p>
+    </article>
+  );
+}
