cdcca2c266388602101697f3590fe7587484311c
Mintaek_CHO <mintaek5555@gmail.com>
2023-07-05 12:52:17 +0900

    Design: Footer 레이아웃 구성

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { AiFillGithub } from "react-icons/ai";
 
 export default function Footer() {
-  return <div className="w-full h-[300px] bg-slate-950 p-8 mt-20">footer</div>;
+  return (
+    <div className="w-full border-t p-8 mt-20">
+      <div className="flex justify-between items-center">
+        <p className="opacity-30">© Copyright 2023 CONCON All rights reserved.</p>
+        <div className="text-4xl opacity-80">
+          <a href="https://github.com/MintaekCho/consert-app" target="_blank">
+            <AiFillGithub />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
