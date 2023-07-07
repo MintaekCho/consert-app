db48d535106a47204370c249eb12f0dbac170bcc
Ko-Eunseo <koyk0408@naver.com>
2023-07-07 20:40:51 +0900

    Design: 모바일 사이즈일때 폰트사이즈 조정

--- src/components/atoms/GuideTxt.tsx
+++ src/components/atoms/GuideTxt.tsx
@@ -2,7 +2,7 @@ import { WrapperProps } from "@/types/_type";
 
 const GuideTxt = ({ children }: WrapperProps) => {
   return (
-    <p className="font-bold text-gray-400 text-xs md:text-md lg:text-lg xl:text-2xl">
+    <p className="font-bold text-gray-400 text-base md:text-md lg:text-lg xl:text-2xl">
       {children}
     </p>
   );
