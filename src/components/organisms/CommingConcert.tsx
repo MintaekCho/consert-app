a5bc46460608d27149ad4e5a7e2624f3ba1bdb49
Mintaek_CHO <mintaek5555@gmail.com>
2023-07-18 14:06:47 +0900

    Fix: Hotrank, Commint 캐싱 무효화처리

--- src/components/organisms/CommingConcert.tsx
+++ src/components/organisms/CommingConcert.tsx
@@ -1,21 +1,21 @@
 "use client";
 import { getApi } from "@/service/api/api";
 import { ConcertData } from "@/types/_type";
-import React, { useEffect } from "react";
-import useSWR, { mutate } from "swr";
+import React, { useState } from "react";
+import useSWR from "swr";
 import Card from "../atoms/Card";
 import Title from "../atoms/Title";
 import Loading from "../common/Loading";
 import CarouselView from "./CarouselView";
 import { BsFillBellFill } from "react-icons/bs";
-import { getStringSelectDate } from "@/utils/date";
 
 export default function CommingConcert() {
-  const timeStamp = new Date().getTime().toString();
-  const { data, error, isLoading } = useSWR(`/api/consert/come`, () =>
-    getApi(`/consert/come?timeStamp=${timeStamp}`)
-  );
+  const [timeStamp, setTimeStamp] = useState(new Date().getTime().toString());
 
+  const { data, error, isLoading } = useSWR(
+    `/api/consert/come?timeStamp=${timeStamp}`,
+    () => getApi(`/consert/come`)
+  );
 
   const commingConcerts: ConcertData[] = data && data.result;
   return (
